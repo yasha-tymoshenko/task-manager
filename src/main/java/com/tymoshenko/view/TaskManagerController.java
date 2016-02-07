cca862c4e4396fc5ee5c2e96c006e1bdef201d55
yasha-tymoshenko <yakiv.tymoshenko@gmail.com>
2016-02-07 03:36:45 +0200

    Create TaskManagerController.java
    This file somehow missing on GIT master branch (but present locally - original branch).

---
+++ src/main/java/com/tymoshenko/view/TaskManagerController.java
@@ -0,0 +1,71 @@
+package com.tymoshenko.view;
+
+import com.tymoshenko.MainApp;
+import com.tymoshenko.model.TaskDto;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+
+/**
+ * @author Yakiv Tymoshenko
+ * @since 05.02.2016
+ */
+public class TaskManagerController {
+
+    private MainApp mainApp;
+
+    @FXML
+    private TableView<TaskDto> taskListTable;
+
+    @FXML
+    private TableColumn<TaskDto, String> nameColumn;
+
+    @FXML
+    private TableColumn<TaskDto, String> pidColumn;
+
+    @FXML
+    private TableColumn<TaskDto, String> memoryUsedColumn;
+
+    @FXML
+    private CheckBox groupByNameCheckBox;
+
+    @FXML
+    private Label tasksNumberLabel;
+
+    @FXML
+    private void initialize() {
+        nameColumn.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
+        pidColumn.setCellValueFactory(cellData -> cellData.getValue().pidProperty());
+        memoryUsedColumn.setCellValueFactory(cellData -> cellData.getValue().memoryProperty());
+    }
+
+    @FXML
+    private void handleShowTasks() {
+        mainApp.refreshTaskList();
+    }
+
+    @FXML
+    private void handleGroupByName() {
+        if (groupByNameCheckBox.isSelected()) {
+            mainApp.groupTasksByName();
+        } else {
+            mainApp.refreshTaskList();
+        }
+    }
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        ObservableList<TaskDto> taskList = mainApp.getTaskList();
+        taskListTable.setItems(taskList);
+        tasksNumberLabel.setText("" + taskList.size());
+        taskList.addListener((ListChangeListener<TaskDto>) c -> {
+            tasksNumberLabel.setText("" + taskList.size());
+        });
+    }
+}
